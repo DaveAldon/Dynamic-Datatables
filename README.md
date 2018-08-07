09f0292479eb464ded83bb6d84b4bcccc5087ffa
DaveAldon <crawford@mail.gvsu.edu>
2018-08-07 14:09:36 -0400

    Initial commit of working files and readme. Working on troubleshooting ajax and adding more extensibility.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Dynamic-Datatables
 Javascript datatables with a bit more dynamism for easy changes and re-usability.
+
+A problem with datatables in many cases is that you have to define your columns and data in more than one location. 
+Usually it's in the html structure and the ajax call. My goal has always been to subdue the logic and make data
+defined in only one location, but still allow flexibility for overriding default content, adding call backs,
+and basically whatever you're used to doing when manipulating datatable output.
+
+But we also don't want to care about changing anything on the source end, because not everyone can dictate in what
+format they'll receive their data.
+
+Currently our only data definition is a javascript dictionary, which can contain any amount of information on specific
+columns if we so choose. In this example, our value of the dictionary is the display value for our column.
+
+var dynamicColumns = {
+	"id":[],
+	"name":["Patient Name"],
+}
+
+If it's blank, it defaults to the column's actual name. In the future, I'll be adding more complexity to this method
+and make it better and easier to throw in a datatable and change its columns on the fly, by simply plopping in
+the tag we know and love, without anything inside of it:
+
+<table id="dataTable" class="display compact wrap" cellspacing="0" width="100%"></table>
---
+++ dynamicDatatables.js
@@ -0,0 +1,64 @@
+var dynamicColumns = {
+	// Column definitions use this pattern: "Name of Column from Ajax":[0 if visible/1 for invisible, "Displayed Column Name"]
+	"id":[],
+	"name":["Patient Name"],
+}
+
+function initDataTable() {
+	table = $('#dataTable').DataTable( {
+		paging: true,
+		orderCellsTop: true,
+		"oLanguage": {
+			"sSearch": "",
+			"sEmptyTable": "No Data found based on parameters"
+		},
+		"sDom": '<"top"lpif>t<"bottom"><"clear">',
+		"ajax": "data/objects.txt",
+		"columns": buildDynamicDataTable($('#dataTable'), dynamicColumns),
+		// Conditional formatting for undelivered messages to look grayed out
+		rowCallback: function(row, data, index) {
+			
+		}
+	});
+}
+
+function buildDynamicDataTable(table, list) {
+	var dict = list
+	var ths;
+	// Build headers
+	for (var key in dict){
+
+		if (typeof list[key][0] == 'undefined') {
+			ths += "<th>"+key+"</th>"
+		}
+		else ths += "<th>"+list[key]+"</th>"
+	}
+	// Build footers
+	var foot = "<tfoot><tr>"
+	for (var key in dict){
+		if (typeof list[key][0] == 'undefined') {
+			foot += "<th>"+key+"</th>"
+		}
+		else foot += "<th>"+list[key]+"</th>"
+	}
+	foot += "</tr></tfoot>"
+		
+	table.append("<thead></thead>").append("<tr></tr>").append(ths)
+	table.append(foot)
+	
+	// Programatic generation of datatable columns
+	var columns = '[{"column":['
+	for(var key in list) {
+	    columns += '{data:"'+key+'"},'
+	}
+	columns = columns.slice(0, -1)
+	columns += ']}]'
+	columns = eval(columns)
+
+	var dynamicColumns = []
+
+	columns[0].column.forEach(function(i) {
+		dynamicColumns.push(i)
+	})
+	return dynamicColumns
+}
